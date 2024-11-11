27577213a6c5edf8dd1ad64d7b247b334b68c516
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-11-11 21:37:18 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" BasicAuth class that inherits from Auth """
+
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class that inherits from Auth """
+    pass
